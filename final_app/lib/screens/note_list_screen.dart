1f63fdfe4e53549bc926853e88a730d6f52ce169
Jahangir Younas <74153084+Jahangir-coder@users.noreply.github.com>
2021-01-09 00:15:33 +0500

    update notes commit done

---
+++ final_app/lib/screens/note_list_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class NoteListScreen extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder();
+  }
+  _launchUrl() async {
+    const url = 'https://www.youtube.com';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+  Widget header() {
+    return GestureDetector(
+      onTap: _launchUrl,
+      child: Container(
+        decoration: BoxDecoration(
+          color: headerColor,
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(75.0),
+          ),
+        ),
+        height: 150,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'ANDROIDRIDE\'S',
+              style: headerRideStyle,
+            ),
+            Text(
+              'NOTES',
+              style: headerNotesStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+    Widget build(BuildContext context) {
+      return FutureBuilder(
+        future: Provider.of<NoteProvider>(context,listen: false).getNotes(),
+        builder: (context,snapshot)
+        {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          else
+          {
+            if(snapshot.connectionState == ConnectionState.done)
+            {
+              return Scaffold(
+                body: Consumer<NoteProvider>(
+                  child: noNotesUI(context),
+                  builder: (context, noteprovider, child) =>
+                  noteprovider.items.length <= 0
+                      ? child
+                      : Container(),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    goToNoteEditScreen(context);
+                  },
+                  child: Icon(Icons.add),
+                ),
+              );
+            }
+            return Container(
+              width: 0.0,
+              height: 0.0,
+            );
+          }
+        },
+      );
+    }
+  }
+}
