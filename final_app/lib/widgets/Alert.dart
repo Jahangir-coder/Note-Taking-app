1927d6759d8f5838b9c7c129e3e92adae2690487
Jahangir Younas <74153084+Jahangir-coder@users.noreply.github.com>
2021-01-09 14:21:20 +0500

    Alert added

---
+++ final_app/lib/widgets/Alert.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notes/helper/note_provider.dart';
+import 'package:flutter_notes/screens/note_edit_screen.dart';
+import 'package:flutter_notes/screens/note_view_screen.dart';
+import 'package:provider/provider.dart';
+import '../utils/constants.dart';
+class ListItem extends StatelessWidget {
+  final int id;
+  final String title;
+  final String content;
+  final String imagePath;
+  final String date;
+  ListItem({this.id, this.title, this.content, this.imagePath, this.date});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 140.0,
+      margin: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, NoteViewScreen.route, arguments: id);
+        },
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 14.0),
+          decoration: BoxDecoration(
+            color: Colors.white38,
+            boxShadow: shadow,
+            borderRadius: BorderRadius.circular(17.0),
+            border: Border.all(color: grey2, width: 1.5),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: itemTitle,
+                      ),
+                      SizedBox(height: 5.0),
+                      Text(
+                        date,
+                        overflow: TextOverflow.ellipsis,
+                        style: itemDateStyle,
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Expanded(
+                        child: Text(
+                          content,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: itemContentStyle,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              if(imagePath!=null)
+              Row(
+                children: [
+                  SizedBox(
+                    width: 14.0,
+                  ),
+                    Container(
+                      width: 85.0,
+                      height: 100.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(13.0),
+                        image: DecorationImage(
+                          image: FileImage(
+                            File(imagePath),
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
